d3be6c49cb147bb3c6471c124edb293f5ef55b0a
Gabor Dolla <agdolla@gmail.com>
2015-04-08 11:46:29 +0200

    fix _generated_ node setup

--- test/setup/node.js
+++ test/setup/node.js
@@ -1,6 +1,8 @@
 var setup = require('./setup');
 var config = require('../../config');
 
+require('babel-core/polyfill.js');
+
 global[config.exportVarName] = require('../../src/' + config.entryFileName);
 global.chai = require('chai');
 global.sinon = require('sinon');
